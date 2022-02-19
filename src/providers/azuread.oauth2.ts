dd66a88e92755ed44ea4990f3b9e7197eb115b9e
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-02-19 17:28:46 +0100

    dev: better error management

--- src/providers/azuread.oauth2.ts
+++ src/providers/azuread.oauth2.ts
@@ -12,12 +12,19 @@ export class AzureADOAuth2 extends OAuth2 {
       code: '' + requestToken,
       redirect_uri: origin + '/api/oauth2/redirect/AZUREAD',
     };
-    const data: {access_token: string} = await got(url, {
-      method: 'POST',
-      form: body,
-      // throwHttpErrors: false,
-    }).json();
-    return data.access_token;
+    try {
+      const data: {access_token: string} = await got(url, {
+        method: 'POST',
+        form: body,
+        // throwHttpErrors: false,
+      }).json();
+      return data.access_token;
+    } catch (err) {
+      console.error('Microsoft API returns an error on url: ', url);
+      console.error('body', body);
+      console.error('err: ', err);
+      throw err;
+    }
   }
 
   getAuthorizeUrl(origin: string): string {
@@ -29,24 +36,32 @@ export class AzureADOAuth2 extends OAuth2 {
   }
 
   async getUserInfo(accessToken: string): Promise<User> {
-    const data = await got
-      .get('https://graph.microsoft.com/v1.0/me', {
-        headers: {
-          // Include the token in the Authorization header
-          Authorization: 'Bearer ' + accessToken,
-        },
-      })
-      .json<{
-        displayName: string;
-        userPrincipalName: string;
-        surname: string;
-      }>();
-    const user = {
-      displayName: data.displayName,
-      email: data.userPrincipalName,
-      id: data.surname,
-      identityProvider: 'azure AD',
-    };
-    return user;
+    const url = 'https://graph.microsoft.com/v1.0/me';
+    try {
+      const data = await got
+        .get(url, {
+          headers: {
+            // Include the token in the Authorization header
+            Authorization: 'Bearer ' + accessToken,
+          },
+        })
+        .json<{
+          displayName: string;
+          userPrincipalName: string;
+          surname: string;
+        }>();
+      const user = {
+        displayName: data.displayName,
+        email: data.userPrincipalName,
+        id: data.surname,
+        identityProvider: 'azure AD',
+      };
+      return user;
+    } catch (err) {
+      console.error('error while calling url: ', url);
+      console.error('accessToken: ', accessToken);
+      console.error('err: ', err);
+      throw err;
+    }
   }
 }
